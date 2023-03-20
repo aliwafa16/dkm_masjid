80692508c8f5fb46f5507a1939ee05084f91c3f9
ezaaa12 <efrizapandia122@gmail.com>
2023-03-20 21:31:50 +0700

    tampilan smart tv

---
+++ application/views/kegiatan/kegiatan.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>DKM Masjid</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="<?= base_url() ?>assets/img/favicon.png" rel="icon">
+    <link href="<?= base_url() ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?= base_url() ?>assets/css/style.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/css/mycss.css" rel="stylesheet">
+
+    <!-- 
+        Updated: Mar 10 2023 with Bootstrap v5.2.3
+     -->
+
+</head>
+
+<body>
+
+    <!-- Content -->
+
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row position-relative">
+                    <div class="background">
+                        <img src="<?= base_url() ?>assets/img/bg_ramadhan2.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-6 kotakthumb position-absolute top-50 end-0 translate-middle-y">
+                        <img src="<?= base_url() ?>assets/img/ustadnih.png" class=" thumbnail rounded float-end me-auto" alt="...">
+                    </div>
+                    <div class="col-6 judulkegiatan position-absolute top-0 start-0">
+                        <div class="kegiatan1">
+                            <h1>SELASA</h1>
+                            <h3>Kajian Ba'da Subuh s.d Syuruq <br>Tafsir Al Qur'an</h3>
+                            <p>Ustd. Dr. Ade Wahidin, Lc, MA</p>
+                        </div>
+                        <div class="kegiatan2">
+                            <h3>Tahsin Tilawah Dewasa Akhwat</h3>
+                            <p>Pagi Jam 08.00 - 10.00</p>
+                            <div class="ustazah">
+                                <p>Ustzh. Zita</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item ">
+                <div class="row position-relative">
+                    <div class="background">
+                        <img src="<?= base_url() ?>assets/img/bg_ramadhan.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-6 kotakthumb position-absolute top-50 end-0 translate-middle-y">
+                        <img src="<?= base_url() ?>assets/img/ustadnih.png" class=" thumbnail rounded float-end me-auto" alt="...">
+                    </div>
+                    <div class="col-6 judulkegiatan position-absolute top-0 start-0">
+                        <div class="kegiatan1">
+                            <h1>SELASA</h1>
+                            <h3>Kajian Ba'da Subuh s.d Syuruq <br>Tafsir Al Qur'an</h3>
+                            <p>Ustd. Dr. Ade Wahidin, Lc, MA</p>
+                        </div>
+                        <div class="kegiatan2">
+                            <h3>Tahsin Tilawah Dewasa Akhwat</h3>
+                            <p>Pagi Jam 08.00 - 10.00</p>
+                            <div class="ustazah">
+                                <p>Ustzh. Zita</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="areamarq position-absolute top-100 start-50 translate-middle">
+                <marquee>Welcome</marquee>
+            </div>
+        </div>
+    </div>
+    
+
+
+    <!-- Vendor JS Files -->
+    <script src="<?= base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="<?= base_url() ?>assets/js/main.js"></script>
+
+</body>
+
+</html>
