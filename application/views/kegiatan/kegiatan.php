a71d3a21ca10e51247580097e4103d89918307b4
ezaaa12 <efrizapandia122@gmail.com>
2023-03-22 21:17:53 +0700

    update tampilan slide

--- application/views/kegiatan/kegiatan.php
+++ application/views/kegiatan/kegiatan.php
@@ -36,7 +36,7 @@
 
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active">
+            <div class="carousel-item active" data-bs-interval="3000">
                 <div class="row position-relative">
                     <div class="background">
                         <img src="<?= base_url() ?>assets/img/bg_ramadhan2.jpg" class="d-block w-100" alt="...">
@@ -44,23 +44,47 @@
                     <div class="col-6 kotakthumb position-absolute top-50 end-0 translate-middle-y">
                         <img src="<?= base_url() ?>assets/img/ustadnih.png" class=" thumbnail rounded float-end me-auto" alt="...">
                     </div>
-                    <div class="col-6 judulkegiatan position-absolute top-0 start-0">
+                    <div class="col-6 d-flex judulkegiatan judulkegiatan1 position-absolute top-50 start-0 translate-middle">
                         <div class="kegiatan1">
+                            <h1>SENIN</h1>
+                            <h3>Kajian bada subuh s.d Syuruq <br>Tadabur Al Qur'an</h3>
+                            <p class="lh-1">Ustd Ali Shodiqin, Lc.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="3000">
+                <div class="row position-relative">
+                    <div class="background">
+                        <img src="<?= base_url() ?>assets/img/bg_ramadhan.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-6 kotakthumb position-absolute top-50 end-0 translate-middle-y">
+                        <img src="<?= base_url() ?>assets/img/ustadnih.png" class=" thumbnail rounded float-end me-auto" alt="...">
+                    </div>
+                    <div class="col-6 judulkegiatan position-absolute top-0 start-0">
+                        <div class="selasa1">
                             <h1>SELASA</h1>
                             <h3>Kajian Ba'da Subuh s.d Syuruq <br>Tafsir Al Qur'an</h3>
-                            <p>Ustd. Dr. Ade Wahidin, Lc, MA</p>
+                            <p class="lh-1">Ustd. Dr. Ade Wahidin, Lc, MA</p>
                         </div>
-                        <div class="kegiatan2">
+                        <div class="selasa1">
                             <h3>Tahsin Tilawah Dewasa Akhwat</h3>
-                            <p>Pagi Jam 08.00 - 10.00</p>
+                            <p class="lh-1">Pagi Jam 08.00 - 10.00</p>
                             <div class="ustazah">
-                                <p>Ustzh. Zita</p>
+                                <p class="lh-1">Ustzh. Zita</p>
+                            </div>
+                        </div>
+                        <div class="selasa1">
+                            <h3>Diskusi Sejarah Islam</h3>
+                            <p class="lh-1">Bada Isya - 21.00</p>
+                            <div class="ustazah">
+                                <p class="lh-1">Ustd Ali Shodiqin, Lc</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="carousel-item ">
+            <div class="carousel-item " data-bs-interval="3000">
                 <div class="row position-relative">
                     <div class="background">
                         <img src="<?= base_url() ?>assets/img/bg_ramadhan.jpg" class="d-block w-100" alt="...">
@@ -70,22 +94,70 @@
                     </div>
                     <div class="col-6 judulkegiatan position-absolute top-0 start-0">
                         <div class="kegiatan1">
-                            <h1>SELASA</h1>
-                            <h3>Kajian Ba'da Subuh s.d Syuruq <br>Tafsir Al Qur'an</h3>
-                            <p>Ustd. Dr. Ade Wahidin, Lc, MA</p>
+                            <h1>RABU</h1>
+                            <h3>Kajian bada subuh s.d Syuruq <br>Tadabur Al Qur'an</h3>
+                            <p class="lh-1">Ustd Ali Shodiqin, Lc</p>
                         </div>
                         <div class="kegiatan2">
-                            <h3>Tahsin Tilawah Dewasa Akhwat</h3>
-                            <p>Pagi Jam 08.00 - 10.00</p>
+                            <h3>Kajian Pagi Khusus Ummahatt <br>Tadabbur Surah An-Nur</h3>
+                            <p class="lh-1">Pagi Jam 08.00 - 10.00</p>
                             <div class="ustazah">
-                                <p>Ustzh. Zita</p>
+                                <p class="lh-1">Ustd Ali Shodiqin, Lc</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item " data-bs-interval="3000">
+                <div class="row position-relative">
+                    <div class="background">
+                        <img src="<?= base_url() ?>assets/img/bg_ramadhan.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-4 kotakthumb position-absolute top-50 end-0 translate-middle-y">
+                        <img src="<?= base_url() ?>assets/img/ustadnih.png" class=" thumbnail rounded float-end me-auto" alt="...">
+                    </div>
+                    <div class="col-8 judulkegiatan position-absolute top-0 start-0">
+                        <div class="kegiatan1">
+                            <h1>KAMIS</h1>
+                            <h3>Kajian bada subuh s.d Syuruq<br>Kajian Mu’amalah Kitab Al-Mu’amalah Al-Mashrafiyyah</h3>
+                            <p class="lh-1">Ustd. Dr. Ade Wahidin, Lc, MA</p>
+                        </div>
+                        <div class="kegiatan2">
+                            <h3>Diskusi Fiqih Islam</h3>
+                            <p class="lh-1">Bada Isya - 21.00</p>
+                            <div class="ustazah">
+                                <p class="lh-1">Ustd Ali Shodiqin, Lc.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item " data-bs-interval="3000">
+                <div class="row position-relative">
+                    <div class="background">
+                        <img src="<?= base_url() ?>assets/img/bg_ramadhan.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-4 kotakthumb position-absolute top-50 end-0 translate-middle-y">
+                        <img src="<?= base_url() ?>assets/img/ustadnih.png" class=" thumbnail rounded float-end me-auto" alt="...">
+                    </div>
+                    <div class="col-8 judulkegiatan position-absolute top-0 start-0">
+                        <div class="kegiatan1">
+                            <h1>JUMAT</h1>
+                            <h3>Kajian bada subuh s.d Syuruq<br>Tadabur Al Qur'an</h3>
+                            <p class="lh-1">Ustd Ali Shodiqin, Lc.</p>
+                        </div>
+                        <div class="kegiatan2">
+                            <h3>Bela Diri Kungfu Muslim</h3>
+                            <p class="lh-1">Bada Ashar  - 17.30</p>
+                            <div class="ustazah">
+                                <p class="lh-1">TAKUMU</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="areamarq position-absolute top-100 start-50 translate-middle">
-                <marquee>Welcome</marquee>
+                <marquee>REKENING AL FURQON: (INFAQ) Bank Syariah Indonesia No Rek: 2021 2026 13 a.n Masjid Al Furqon (SANTUNAN ANAK YATIM) Bank Syariah Indonesia No Rek: 2021 2026 35 (WAKAF) Bank Syariah Indonesia No Rek: 2021 2026 46 </marquee>
             </div>
         </div>
     </div>
