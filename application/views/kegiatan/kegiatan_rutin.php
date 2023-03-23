6a2c7bd3631da1f9ce0ac53fd89e6de2ffd8403d
nadiaamandafeb22 <nadiafebriantuy@gmail.com>
2023-03-23 10:13:59 +0700

    kegiatan rutin

---
+++ application/views/kegiatan/kegiatan_rutin.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>DKM Masjid</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="<?= base_url() ?>assets/img/favicon.png" rel="icon">
+    <link href="<?= base_url() ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?= base_url() ?>assets/css/style.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/css/mycss.css" rel="stylesheet">
+
+    <!-- 
+        Updated: Mar 10 2023 with Bootstrap v5.2.3
+     -->
+
+     <style>
+        .judulkegiatan{
+            margin-top: 80px;
+        }
+
+        .carousel-item .thumbnail {
+            width: 500px;
+            height: 350px !important;
+            margin-top: -50px;
+        }
+     </style>
+
+</head>
+
+<body>
+
+    <!-- Content -->
+
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row position-relative">
+                    <div class="background">
+                        <img src="<?= base_url() ?>assets/img/bg_ramadhan.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-6 kotakthumb position-absolute top-50 end-0 translate-middle-y">
+                        <img src="<?= base_url() ?>assets/img/kegiatanmasjid/masjid.png" class=" thumbnail rounded float-end me-auto">
+                    </div>
+                    <div class="col-6 judulkegiatan position-absolute top-0 start-0">
+                        <div class="kegiatan1">
+                            <h3>KEGIATAN ASMAF SANTUNAN ANAK YATIM</h3>
+                            <h5 style="color: #fff;">Santunan kepada 130 Anak Yatim</h5>
+                        </div>
+                        <div class="kegiatan2">
+                            <p>Bank Syariah Indonesia atas nama Masjid Alfurqon</p>
+                            <div class="ustazah">
+                                <p>No Rekening : 2021202635</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item ">
+            <div class="row position-relative">
+                    <div class="background">
+                        <img src="<?= base_url() ?>assets/img/bg_ramadhan2.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-6 kotakthumb position-absolute top-50 end-0 translate-middle-y">
+                        <img src="<?= base_url() ?>assets/img/kegiatanmasjid/masjid.png" class=" thumbnail rounded float-end me-auto">
+                    </div>
+                    <div class="col-6 judulkegiatan position-absolute top-0 start-0">
+                        <div class="kegiatan1">
+                            <h3>WAKAF PEMBANGUNAN AREA PARKIR DAN KELAS</h3>
+                             <h5 style="color: #fff;">Dana yang dibutuhkan untuk pembangunan area parkir motor dan kelas sebesar Rp 815 juta.
+                             </h5>
+                        </div>
+                        <div class="kegiatan2">
+                            <p>Bank Syariah Indonesia atas nama Masjid Alfurqon</p>
+                            <div class="ustazah">
+                                <p>No Rekening : 2021202646</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item">
+            <div class="row position-relative">
+                    <div class="background">
+                        <img src="<?= base_url() ?>assets/img/bg_ramadhan.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-6 kotakthumb position-absolute top-50 end-0 translate-middle-y">
+                        <img src="<?= base_url() ?>assets/img/kegiatanmasjid/masjid.png" class=" thumbnail rounded float-end me-auto">
+                    </div>
+                    <div class="col-6 judulkegiatan position-absolute top-0 start-0">
+                        <div class="kegiatan1" style="margin-top: -30px;">
+                            <h3>WAKAF PUING DAN TANAMAN</h3>
+                             <h5 style="color: #fff;">Masjid Al Furqon menerima WAKAF PUING bekas bangunan, untuk meratakan tanah di samping barat masjid AF dan WAKAF BIBIT TANAMAN untuk menghijaukan lingkungan sekitar masjid. BIBIT TANAMAN yang dibutuhkan:
+                             </h5>
+                             <ul style="color:#fff; font-size:15px;">
+                                <li>Kelapa Gading</li>
+                                <li>Melati</li>
+                                <li>Jambu Jamaika</li>
+                                <li>Delima</li>
+                                <li>Daun Mentega</li>
+                                <li>Roelia</li>
+                             </ul>
+                        </div>
+                        <div class="kegiatan2" style="margin-top: 40px;">
+                            <p>Bank Syariah Indonesia atas nama Masjid Alfurqon</p>
+                            <div class="ustazah">
+                                <p style="font-size: 15px;">No Rekening : 2021202646</p>
+                                <p style="font-size: 15px;">No Rekening : 20212026-13 (Operasional)</p>
+                                <p style="font-size: 15px;">No Rekening : 20212026-24 (ZIS)</p>
+                                <p style="font-size: 15px;">No Rekening : 20212026-35 (ASMAF/Keg sosial)</p>
+                                <p style="font-size: 15px;">No Rekening : 20212026-46 (Wakaf)</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <div class="areamarq position-absolute top-100 start-50 translate-middle">
+                <marquee>Welcome</marquee>
+            </div> -->
+        </div>
+    </div>
+    
+
+
+    <!-- Vendor JS Files -->
+    <script src="<?= base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="<?= base_url() ?>assets/js/main.js"></script>
+
+</body>
+
+</html>
